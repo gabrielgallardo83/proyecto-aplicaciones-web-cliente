b15df01d0c9bd85eb75c05009fa2d786a0543bc3
Gabriel_Gallardo <gabriel.gallardo@istea.com.ar>
2025-05-03 21:20:56 -0300

    ajustes generales

--- js/script.js
+++ js/script.js
@@ -106,3 +106,26 @@ var swiper = new Swiper(".arrivals-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".blogs-slider", {
+  spaceBetween: 10,
+  loop: true,
+  centerSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
